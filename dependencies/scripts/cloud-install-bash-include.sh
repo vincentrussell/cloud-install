ae016c6ef7711f66a5c5ebdfe0e4ae6ff6bf6b5b
Kurt Vangraefschepe <kvangrae@wavestrike.com>
2014-05-30 11:04:05 -0400

    added hadoop/zookeeper/accumulo bin directory to PATHS environment variable

--- dependencies/scripts/cloud-install-bash-include.sh
+++ dependencies/scripts/cloud-install-bash-include.sh
@@ -1,6 +1,7 @@
-
 export HADOOP_HOME="/path/to/hadoop"
 export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
 export HADOOP_PREFIX=$HADOOP_HOME
 export ACCUMULO_HOME="/path/to/accumulo"
 export ZOOKEEPER_HOME="/path/to/zookeeper"
+
+export PATH=$PATH:${HADOOP_HOME}/bin:${ZOOKEEPER_HOME}/bin:${ACCUMULO_HOME}/bin
